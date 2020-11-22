1df7cf479d4a338848486f388e8ee6b0493759f0
svg94 <liga-2000@hotmail.de>
2020-11-22 17:14:22 +0100

    Function in Function stuff

--- U4/4.1.js
+++ U4/4.1.js
@@ -6,3 +6,24 @@ function identity_function(x){
         return x;
     }
 }
+function add(x,y){
+    return x+y;
+}
+function mul(x,y){
+    return x*y;
+}
+function addf(x){
+    return function(y){
+        return x+y;
+    }
+}
+function applyf(op){
+    return addf();
+}
+function applyf(op){
+    return function(x){
+        return function (y){
+            return op(x,y);
+        }
+    }
+}
