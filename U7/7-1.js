e35563e4be925cbd7ab81c7f427a5e579fb5f329
svg94 <liga-2000@hotmail.de>
2020-12-17 19:53:44 +0100

    7-1 finished

--- U7/7-1.js
+++ U7/7-1.js
@@ -1,7 +1,8 @@
-const testPage = document.getElementById("testID")
-
+const testPage = document.getElementById("testID");
+const n = 10000;
+let load;
 let t0 = performance.now();
-let load = testPage.innerHTML;
+load = testPage.innerHTML;
 const t_innerHTML = performance.now() - t0;
 
 t0 = performance.now();
@@ -16,8 +17,40 @@ t0 = performance.now();
 load = testPage.outerHTML;
 const t_outerHTML = performance.now() - t0;
 
+t0 = performance.now();
+for(let i = 0; i < n; i++){
+    load = testPage.innerHTML;
+}
+const t_BetterInnerHTML = performance.now() - t0;
+
+t0 = performance.now();
+for(let i = 0; i < n; i++){
+    load = testPage.innerText;
+}
+const t_BetterInnerText = performance.now() - t0;
+
+t0 = performance.now();
+for(let i = 0; i < n; i++){
+    load = testPage.textContent;
+}
+const t_BetterTextContent = performance.now() - t0;
+
+t0 = performance.now();
+for(let i = 0; i < n; i++){
+    load = testPage.outerHTML;
+}
+const t_BetterOuterHTML = performance.now() - t0;
+
 document.getElementById('valueInnerHTML').innerText = "" + t_innerHTML.toFixed(2) + "ms";
 document.getElementById('valueInnerText').innerText = "" + t_innerText.toFixed(2) + "ms";
 document.getElementById('valueTextContent').innerText = "" + t_textContent.toFixed(2) + "ms";
 document.getElementById('valueOuterHTML').innerText = "" + t_outerHTML.toFixed(2) + "ms";
+
+document.getElementById('BetterValueInnerHTML').innerText = "" + t_BetterInnerHTML.toFixed(2) + "ms";
+document.getElementById('BetterValueInnerText').innerText = "" + t_BetterInnerText.toFixed(2) + "ms";
+document.getElementById('BetterValueTextContent').innerText = "" + t_BetterTextContent.toFixed(2) + "ms";
+document.getElementById('BetterValueOuterHTML').innerText = "" + t_BetterOuterHTML.toFixed(2) + "ms";
+
+document.getElementById('headerOfBetterTable').innerText = "More interesting results because it's times "+n;
 console.log("DONE");
+
